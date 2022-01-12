7de349a11c3f0075961ce4f6e596a28a99b8dfe1
tarabite <88362366+tarabite@users.noreply.github.com>
2022-01-12 00:08:47 -0800

    Tested different max widths, but staying with 500px for now

--- src/Photos.js
+++ src/Photos.js
@@ -3,28 +3,24 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-  console.log(props.photos);
-
   if (props.photos) {
     return (
       <div className="Photos">
-        <div className="container">
-          <div className="row">
-            {props.photos.map(function (photo, index) {
-              return (
-                <div className="col-6 fluid" key={index}>
-                  <a href={photo.src.original} target="blank" rel="roreferrer">
-                    <img
-                      src={photo.src.tiny}
-                      className="image-fluid"
-                      alt={photo.alt}
-                      title={photo.alt}
-                    />
-                  </a>
-                </div>
-              );
-            })}
-          </div>
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-6" key={index}>
+                <a href={photo.src.original} target="blank" rel="roreferrer">
+                  <img
+                    src={photo.src.tiny}
+                    className="image-fluid"
+                    alt={photo.src.photographer}
+                    title={photo.alt}
+                  />
+                </a>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
