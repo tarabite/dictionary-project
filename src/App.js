cea695948654caff10944da669d31c7ac0215114
tarabite <88362366+tarabite@users.noreply.github.com>
2022-01-16 17:40:13 -0800

    Creating link to Weather app in footer

--- src/App.js
+++ src/App.js
@@ -13,25 +13,42 @@ export default function App() {
       </div>
       <footer className="App-footer">
         <small>
-          Coded by Tara Diego. Open-sourced on{" "}
-          <a
-            href="https://github.com/tarabite/dictionary-project"
-            alt="Tara Diego's Dictionary project on GitHub"
-            target="blank"
-            title="Click to visit Tara's GitHub profile"
-          >
-            Github
-          </a>
-          . Hosted on{" "}
-          <a
-            href="https://agitated-spence-006f7c.netlify.app"
-            alt="Copy and share this link from Netlify"
-            target="blank"
-            title="Click to visit Tara's GitHub profile"
-          >
-            Netlify
-          </a>
-          .
+          <div className="Credit">Coded by Tara Diego.</div>
+          <div className="WeatherLink">
+            <a
+              href="https://stupefied-goodall-8299c5.netlify.app"
+              alt="Tara's Weather App built with React"
+              target="blank"
+              title="Click to visit Tara's React Weather App for SheCodes"
+            >
+              <i class="fas fa-cloud-showers-heavy"></i> Check out my React
+              Weather App <i class="fas fa-cloud-showers-heavy"></i>
+            </a>
+          </div>
+          <div className="Github">
+            Open-sourced on{" "}
+            <a
+              href="https://github.com/tarabite/dictionary-project"
+              alt="Tara Diego's Dictionary project on GitHub"
+              target="blank"
+              title="Click to visit Tara's GitHub profile"
+            >
+              Github
+            </a>
+            .
+          </div>
+          <div className="Netlify">
+            Hosted on{" "}
+            <a
+              href="https://agitated-spence-006f7c.netlify.app"
+              alt="Copy and share this link from Netlify"
+              target="blank"
+              title="Click to visit Tara's GitHub profile"
+            >
+              Netlify
+            </a>
+            .
+          </div>
         </small>
       </footer>
     </div>
